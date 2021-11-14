d20a2919156babec9cad59b9bd845672c37b8051
Waleed-Zeddini <waleed.zeddini@gmail.com>
2021-11-14 22:28:04 +0100

    first commit

---
+++ src/test/java/com/nexio/register/erureka/server/NexioRegisterErurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexio.register.erureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexioRegisterErurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
